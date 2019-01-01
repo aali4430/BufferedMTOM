f3475d5b0c77bf6be4269f163651eaa249d36ef5
aali4430 <aali4430@gmail.com>
2019-01-01 14:14:27 -0800

    Add files via upload

---
+++ IUploader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.IO;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IUploader
+    {
+
+        //[OperationContract]
+        //string GetData(int value);
+
+        //[OperationContract]
+        //Uploader GetDataUsingDataContract(Uploader composite);
+        [OperationContract]
+        void Upload(RemoteFileInfo objRemoteFile);
+        // TODO: Add your service operations here
+    }
+    [MessageContract]
+    public class RemoteFileInfo
+    {
+        [MessageHeader]
+        public string FileName { get; set; }
+
+        [MessageBodyMember]
+        public byte[] Data { get; set; }
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    //Following code for streaaming transfer
+    //[MessageContract]
+    //public class RemoteFileInfo
+    //{
+    //    [MessageHeader]
+    //    public string FileName { get; set; }
+
+    //    [MessageBodyMember]
+    //    public Stream Data { get; set; }
+    //}
+}
---
+++ Uploader.svc.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.IO;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Uploader : IUploader
+    {
+        public void Upload(RemoteFileInfo objRemoteFile)
+        {
+
+            string filePath = @"C:\NewFile\" + objRemoteFile.FileName;
+            string filePath2 = @"C:\NewFile\log.txt";
+            int counter = 0;
+            List<string> str = new List<string>();
+            Stream stream = new MemoryStream(objRemoteFile.Data);
+            using (stream) // data is the Stream value.
+            {
+                using (BinaryReader reader = new BinaryReader(stream))
+                {
+                    using (FileStream fs = new FileStream(filePath, FileMode.Append))
+                    {
+                        using (BinaryWriter writer = new BinaryWriter(fs))
+                        {
+                                
+                                byte[] buffer = reader.ReadBytes(4096);
+                                writer.Write(buffer);
+                        }
+                    }
+                }
+            }
+            str.Add(counter.ToString());
+            File.AppendAllLines(filePath2, str);
+            //filewriter.WriteLine(DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss")+"---Counter="+counter.ToString());
+        }
+        [Obsolete]
+        public void Upload2(RemoteFileInfo objRemoteFile)
+        {
+           
+            string filePath = @"C:\NewFile\"+objRemoteFile.FileName;
+            string filePath2 = @"C:\NewFile\log.txt";
+            int counter = 0;
+            List<string> str = new List<string>();
+            Stream stream = new MemoryStream(objRemoteFile.Data);
+            using (stream) // data is the Stream value.
+            {
+                using (BinaryReader reader = new BinaryReader(stream))
+                {
+                    using (FileStream fs = new FileStream(filePath, FileMode.Create))
+                    {
+                        using (BinaryWriter writer = new BinaryWriter(fs))
+                        {
+                            byte[] buffer;
+                            do
+                            {
+                                
+                                str.Add(DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff"));
+                                buffer = reader.ReadBytes(4096);
+                                writer.Write(buffer);
+                                counter += 1;
+                            } while (buffer.Length > 0);
+                        }
+                    }
+                }
+            }
+            str.Add(counter.ToString());
+            File.AppendAllLines(filePath2, str);
+            //filewriter.WriteLine(DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss")+"---Counter="+counter.ToString());
+        }
+    }
+}
